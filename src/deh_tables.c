b3fd7b1677611e5dd0c6a7ab3b6ac09fb0ad1803
Cheariisan <cherry.avium168@gmail.com>
2024-08-28 22:04:31 +0000

    Expose most of the music handler to Lua

--- src/deh_tables.c
+++ src/deh_tables.c
@@ -24,7 +24,8 @@
 #include "g_state.h" // gamestate_t (for lua)
 #include "r_data.h" // patchalphastyle_t
 #include "k_boss.h" // spottype_t (for lua)
-
+#include "k_follower.h" // followermode_t (for lua)
+#include "music.h" // tune flags (for lua)
 #include "deh_tables.h"
 
 char *FREE_STATES[NUMSTATEFREESLOTS];
@@ -5155,6 +5156,16 @@ struct int_const_s const INT_CONST[] = {
 	{"PRECIPFX_THUNDER",PRECIPFX_THUNDER},
 	{"PRECIPFX_LIGHTNING",PRECIPFX_LIGHTNING},
 	{"PRECIPFX_WATERPARTICLES",PRECIPFX_WATERPARTICLES},
+
+	// tune flags
+	{"TN_INCLUSIVEFADE",TN_INCLUSIVEFADE},
+	{"TN_USEMAPVOLUME",TN_USEMAPVOLUME},
+	{"TN_SYNCMUSIC",TN_SYNCMUSIC},
+	{"TN_MUSICCRED",TN_MUSICCRED},
+	{"TN_VAPES",TN_VAPES},
+	{"TN_NIGHTCOREABLE",TN_NIGHTCOREABLE},
+	{"TN_CHANGEPITCH",TN_CHANGEPITCH},
+	{"TN_LOOPING",TN_LOOPING},
 
 	{NULL,0}
 };
