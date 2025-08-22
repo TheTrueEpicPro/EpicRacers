2e77f2ffa60f531c131f7d0ea96a395709f4e0f1
Eidolon <furyhunter600@gmail.com>
2025-08-22 01:24:14 +0000

    Merge branch 'botvarsLib' into 'master'
    Bot library for Lua (botvars getter/setter, bot functions)
    
    See merge request KartKrew/RingRacers!75

--- src/lua_libs.h
+++ src/lua_libs.h
@@ -104,6 +104,17 @@ extern lua_State *gL;
 
 #define META_FOLLOWER "FOLLOWER_T*"
 
+#define META_SONICLOOPVARS "SONICLOOPVARS_T*"
+#define META_SONICLOOPCAMVARS "SONICLOOPCAMVARS_T*"
+
+#define META_BOTVARS "BOTVARS_T*"
+#define META_BOTCONTROLLER "BOTCONTROLLER_T*"
+
+#define META_SPLASH "T_SPLASH_T*"
+#define META_FOOTSTEP "T_FOOTSTEP_T*"
+#define META_OVERLAY "T_OVERLAY_T*"
+#define META_TERRAIN "TERRAIN_T*"
+
 boolean luaL_checkboolean(lua_State *L, int narg);
 
 int LUA_EnumLib(lua_State *L);
@@ -123,6 +134,8 @@ int LUA_PolyObjLib(lua_State *L);
 int LUA_BlockmapLib(lua_State *L);
 int LUA_HudLib(lua_State *L);
 int LUA_FollowerLib(lua_State *L);
+int LUA_BotVarsLib(lua_State *L);
+int LUA_TerrainLib(lua_State *L);
 
 #ifdef __cplusplus
 } // extern "C"
